56442dab3972c2f594f2864430427d2658bf6c32
tareq-360 <mdtareqhossain2002@gmail.com>
2026-04-14 19:45:58 +0600

    initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Countries from './components/countries/countries'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Get started</h1>
+      <Countries></Countries>
+
+
+
+    </>
+  )
+}
+
+export default App
